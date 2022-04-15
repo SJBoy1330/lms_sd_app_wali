bd015468e9a279d687cf9d32d374488ad21f6cb8
saka_mawa <sakadana2003@gmail.com>
2022-04-15 14:07:03 +0700

    Update

---
+++ modules/spp/views/index.php
@@ -0,0 +1,1 @@
+<h5>Halaman Toko</h5>
--- modules/toko/views/index.php
+++ modules/toko/views/index.php
@@ -1,1 +1,51 @@
-Halo Halo
+<!-- Begin page -->
+<main class="h-100">
+
+<!-- Header -->
+<header class="header position-fixed">
+    <div class="row">
+        <div class="col-auto">
+            <a href="javascript:void(0)" target="_self" class="btn btn-44 menu-btn">
+                <img src="../HTML/assets/icons/hamburger.png" width="24" alt="">
+            </a>
+        </div>
+        <div class="col text-center">
+            <!-- <div class="logo-small">
+                <img src="assets/img/logo.png" alt="" />
+                <h5><span class="text-secondary fw-light">Finance</span><br />Wallet</h5>
+            </div> -->
+        </div>
+        <div class="col-auto">
+            <a href="notifikasi-SD.html" target="_self" class="btn btn-44 rounded-circle btn-notifikasi">
+                <img src="../HTML/assets/icons/notif.png" width="24" alt="">
+                <span class="count-indicator"></span>
+            </a>
+        </div>
+    </div>
+</header>
+<!-- Header ends -->
+
+
+<!-- main page content -->
+<div class="main-container container">
+    <div class="col-12 align-self-center pb-3">
+        <div class="row">
+            <div class="col-12 px-0 align-self-center">
+                <div class="row mx-0">
+                    <div class="col-ld-12 position-relative mt-5 d-flex justify-content-center align-items-center">
+                        <img src="<?= base_url('assets/images/development.png')?>" width="275" style="animation: up-down 2s ease-in-out infinite alternate-reverse both;">
+                    </div>
+                    <div>
+                        <h4 class="mt-5 mb-3 text-center text-dark">Tahap Pengembangan</h4>
+                        <p class="text-secondary text-center title-2 mx-2">
+                         Fitur toko belum tersedia. Fitur dalam tahap pengembangan
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- main page content ends -->
+
+</main>
