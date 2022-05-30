4c3024e6988c623561d2c8fddbd8d9b274d7857f
saka_mawa <sakadana2003@gmail.com>
2022-05-30 18:05:14 +0700

    Update Terbaru

--- modules/profil/views/tentang_sekolah.php
+++ modules/profil/views/tentang_sekolah.php
@@ -1,6 +1,5 @@
 <!-- Begin page -->
 <main class="h-100">
-
     <!-- Header -->
     <header class="header position-fixed">
         <div class="row">
@@ -20,99 +19,99 @@
         </div>
     </header>
     <!-- Header ends -->
+    
     <!-- main page content -->
-    <div class="main-container container">
-        <div class="vektor-bantuan-satu">
-            <img src="<?= base_url('assets/images/vektor-bantuan-1.png') ?>" width="175" alt="">
-        </div>
-        <div class="vektor-bantuan-dua">
-            <img src="<?= base_url('assets/images/vektor-bantuan-2.png') ?>" width="175" alt="">
-        </div>
-        <div class="row justify-content-end pe-3">
-            <div class="col-6">
-                <div class="row mb-3 bg-f5f5f5 py-3 rounded-10 rows-1">
-                    <div class="col-auto">
-                        <div class="avatar avatar-40 shadow-sm rounded-circle avatar-presensi-outline">
-                            <div class="avatar avatar-30 rounded-circle avatar-presensi-inline">
-                                <i class="fa-solid fa-school size-15 mt-0 text-white"></i>
+    <div class="main-container container top-20">
+        <div class="row">
+            <div class="col-12 px-0">
+                <div class="box-fiture">
+                    <div class="list-group-item border-0 bg-f5f5f5 shadow-none rounded-15 py-1 px-3 mb-3 mx-3">
+                        <div class="row p-2">
+                            <div class="col-6 align-self-center p-0">
+                                <img src="<?= base_url(); ?>assets/images/tentang_logo_sekolah.svg" alt="" width="175">
+                            </div>
+                            <div class="col-6 align-self-center ps-2">
+                                <p class="mb-0 fw-bold size-16">Logo Sekolah</p>
+                                <p class="text-secondary size-14"><?= $data_sekolah->nama_sekolah ?></p>
+                            </div>
+                            <div class="logo-sekolah position-absolute" style="top: 45px; left: 65px; width:50px;">
+                                <img src="<?= base_url(); ?>assets/images/logo-grafika.png" alt="" width="30">
                             </div>
                         </div>
                     </div>
-                    <div class="col align-self-center ps-0">
-                        <p class="mb-0 size-13 fw-medium">Nama Sekolah</p>
-                        <p class="mb-0 size-10 fw-normal text-secondary"><?= $data_sekolah->nama_sekolah ?></p>
-                    </div>
                 </div>
-                <div class="row mb-3 bg-f5f5f5 py-3 rounded-10 rows-1">
-                    <div class="col-auto">
-                        <div class="avatar avatar-40 shadow-sm rounded-circle avatar-presensi-outline">
-                            <div class="avatar avatar-30 rounded-circle avatar-presensi-inline">
-                                <i class="fa-solid fa-id-badge size-15 mt-0 text-white"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col align-self-center ps-0">
-                        <p class="mb-0 size-13 fw-medium">NPSM</p>
-                        <p class="mb-0 size-10 fw-normal text-secondary"><?= $data_sekolah->npsm ?></p>
-                    </div>
-                </div>
-            </div>
 
-        </div>
-        <div class="row my-5 ps-3 rows-1">
-            <div class="col-6 mb-3 rounded-10 p-2">
-                <div class="row mb-3 bg-f5f5f5 py-3 rounded-10">
-                    <div class="row d-flex justify-content-center align-items-center mb-2">
-                        <div class="col-auto">
-                            <div class="avatar avatar-40 shadow-sm rounded-circle avatar-presensi-outline">
-                                <div class="avatar avatar-30 rounded-circle avatar-presensi-inline">
-                                    <i class="fa-solid fa-school size-15 mt-0 text-white"></i>
-                                </div>
+                <div class="box-fiture">
+                    <div class="list-group-item border-0 bg-f5f5f5 shadow-none rounded-15 py-1 px-3 mb-3 mx-3">
+                        <div class="row p-2">
+                            <div class="col-6 align-self-center ps-3">
+                                <p class="mb-0 fw-bold size-16">Nama Sekolah</p>
+                                <p class="text-secondary size-14"><?= $data_sekolah->nama_sekolah ?></p>
+                            </div>
+                            <div class="col-6 align-self-center p-0">
+                                <img src="<?= base_url(); ?>assets/images/tentang_nama_sekolah.svg" alt="" width="125">
                             </div>
                         </div>
                     </div>
-                    <div class="col-auto align-self-center ps-0">
-                        <p class="mb-0 size-13 fw-medium mb-2 text-center">Alamat Sekolah</p>
-                        <p class="mb-0 size-10 fw-normal text-secondary ps-3"><?= $data_sekolah->alamat ?></p>
-                    </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="row d-flex justify-content-around align-items-center rows-1">
-            <div class="col-5">
-                <div class="row bg-f5f5f5 py-3 rounded-10">
-                    <div class="col-auto ps-2 pe-2">
-                        <div class="avatar avatar-40 shadow-sm rounded-circle avatar-presensi-outline">
-                            <div class="avatar avatar-30 rounded-circle avatar-presensi-inline">
-                                <i class="fa-solid fa-graduation-cap size-15 mt-0 text-white"></i>
+                <div class="box-fiture">
+                    <div class="list-group-item border-0 bg-f5f5f5 shadow-none rounded-15 py-1 px-3 mb-3 mx-3">
+                        <div class="row p-2">
+                            <div class="col-6 align-self-center ps-3">
+                                <img src="<?= base_url(); ?>assets/images/tentang_nspm_sekolah.svg" alt="" width="100">
+                            </div>
+                            <div class="col-6 align-self-center p-0">
+                                <p class="mb-0 fw-bold size-16">NPSM Sekolah</p>
+                                <p class="text-secondary size-14"><?= $data_sekolah->npsm ?></p>
                             </div>
                         </div>
-                    </div>
-                    <div class="col align-self-center ps-0">
-                        <p class="mb-0 size-12 fw-medium">Jumlah Siswa</p>
-                        <p class="mb-0 size-10 fw-normal text-secondary"><?= $count_siswa ?> Siswa</p>
                     </div>
                 </div>
-            </div>
-            <div class="col-5">
-                <div class="row bg-f5f5f5 py-3 rounded-10">
-                    <div class="col-auto ps-2 pe-2">
-                        <div class="avatar avatar-40 shadow-sm rounded-circle avatar-presensi-outline">
-                            <div class="avatar avatar-30 rounded-circle avatar-presensi-inline">
-                                <i class="fa-solid fa-chalkboard-user size-15 mt-0 text-white"></i>
+
+                <div class="box-fiture">
+                    <div class="list-group-item border-0 bg-f5f5f5 shadow-none rounded-15 py-1 px-3 mb-3 mx-3">
+                        <div class="row p-2">
+                            <div class="col-6 align-self-center ps-3">
+                                <p class="mb-0 fw-bold size-16">Alamat Sekolah</p>
+                                <p class="text-secondary size-12"><?= $data_sekolah->alamat ?></p>
+                            </div>
+                            <div class="col-6 align-self-center p-0">
+                                <img src="<?= base_url(); ?>assets/images/tentang_alamat_sekolah.svg" alt="" width="115">
                             </div>
                         </div>
                     </div>
-                    <div class="col align-self-center ps-0">
-                        <p class="mb-0 size-12 fw-medium">Jumlah Staf</p>
-                        <p class="mb-0 size-10 fw-normal text-secondary"><?= $count_staf ?> Staf</p>
+                </div>
+
+                <div class="box-fiture">
+                    <div class="list-group-item border-0 bg-f5f5f5 shadow-none rounded-15 py-1 px-3 mb-3 mx-3">
+                        <div class="row p-2">
+                            <div class="col-6 align-self-center ps-3">
+                                <img src="<?= base_url(); ?>assets/images/tentang_jumlah_siswa.svg" alt="" width="115">
+                            </div>
+                            <div class="col-6 align-self-center p-0">
+                                <p class="mb-0 fw-bold size-16">Jumlah Siswa</p>
+                                <p class="text-secondary size-14"><?= $count_siswa ?> Siswa</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box-fiture">
+                    <div class="list-group-item border-0 bg-f5f5f5 shadow-none rounded-15 py-1 px-3 mb-3 mx-3">
+                        <div class="row p-2">
+                            <div class="col-6 align-self-center ps-3">
+                                <p class="mb-0 fw-bold size-16">Jumlah Staf</p>
+                                <p class="text-secondary size-14"><?= $count_staf ?> Staf</p>
+                            </div>
+                            <div class="col-6 align-self-center p-0">
+                                <img src="<?= base_url(); ?>assets/images/tentang_jumlah_staf.svg" alt="" width="115">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
     <!-- main page content ends -->
-
 </main>
