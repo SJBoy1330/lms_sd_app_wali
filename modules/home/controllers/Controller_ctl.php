ccc4c3dff02110b7d84c9d6b4ce188254acf1d29
jeje <jjeje1303@gmail.com>
2022-05-30 21:40:35 +0700

    PUSH KATEGORI LIST BERITA

--- modules/home/controllers/Controller_ctl.php
+++ modules/home/controllers/Controller_ctl.php
@@ -136,7 +136,7 @@ class Controller_ctl extends MY_Frontend
 		$this->data['css_add'][] = '<link rel="stylesheet" href="' . base_url('assets/css/style-wali.css') . '">';
 
 		// LOAD JS
-		$this->data['js_add'][] = '<script src="' . base_url() . 'assets/js/page/berita/listberita.js"></script>';
+		$this->data['js_add'][] = '<script src="' . base_url() . 'assets/js/page/berita/berita.js"></script>';
 
 		// meta data
 		$id_sekolah = $this->session->userdata('lms_wali_id_sekolah');
--- modules/home/views/list_berita.php
+++ modules/home/views/list_berita.php
@@ -2,55 +2,55 @@
 <div class="main-container container top-20">
     <div class="row">
         <div class="col-12 col-md-10 col-lg-8 mx-auto">
-            <!-- List Berita -->
-            <div class="row mb-3">
-                <div class="col">
-                    <h6 class="title">Berita Hari Ini</h6>
-                </div>
-                <div class="col-auto">
-                    <!-- <a href="bills.html" class="small">Pay Bill</a> -->
-                </div>
-            </div>
+            <?php if (isset($data_berita) && $data_berita != NULL) : ?>
 
-            <div class="row mb-2">
-                <div class="col-12 px-0">
-                    <!-- swiper categories -->
-                    <div class="swiper-container connectionwiper">
-                        <div class="swiper-wrapper">
-                            <a class="swiper-slide" href="<?= base_url('home/list_berita') ?>">
-                                <div class="tag border <?= !isset($_GET['kategori']) ? 'active' : '' ?>">
-                                    <span class="text-uppercase">Semua</span>
-                                </div>
-                            </a>
-                            <?php foreach ($data_kategori as $k) : ?>
-                                <a class="swiper-slide" href="<?= base_url('home/list_berita?kategori=' . $k->id_kategori_konten) ?>">
-                                    <div class="tag border <?= $_GET['kategori'] == $k->id_kategori_konten ? 'active' : '' ?>">
-                                        <span class="text-uppercase"><?= $k->nama ?></span>
+                <div class="row mb-2">
+                    <div class="col-12 px-0">
+                        <!-- swiper categories -->
+                        <div class="swiper-container connectionwiper">
+                            <div class="swiper-wrapper base_kategori">
+                                <a class="swiper-slide" onclick="get_kategori(this,'all')">
+                                    <div class="tag border kategori active">
+                                        <span class="text-uppercase">Semua</span>
                                     </div>
                                 </a>
-                            <?php endforeach; ?>
+                                <?php foreach ($data_kategori as $k) : ?>
+                                    <a class="swiper-slide" onclick="get_kategori(this,<?= $k->id_kategori_konten; ?>)">
+                                        <div class="tag border kategori">
+                                            <span class="text-uppercase"><?= $k->nama ?></span>
+                                        </div>
+                                    </a>
+                                <?php endforeach; ?>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div id="display_berita">
+                    <?php foreach ($data_berita as $b) : ?>
+                        <div data-kategori="kategori-<?= $b->id_kategori_konten; ?>" class="zoom-filter">
+                            <a href="<?= base_url('home/detail_berita/') . $b->id_konten ?>" class="col-12 text-dark">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <figure class="overflow-hidden rounded-15 text-center detail-berita" style="background-position: center; background-size: cover; background-image: url('<?= $b->gambar != '' || $b->gambar != NULL ? $b->gambar : 'kosong' ?>');">
+                                        </figure>
+                                    </div>
+                                </div>
 
-            <?php foreach ($data_berita as $b) : ?>
-                <a href="<?= base_url('home/detail_berita/') . $b->id_konten ?>" class="col-12 text-dark">
-                    <div class="row">
-                        <div class="col-12">
-                            <figure class="overflow-hidden rounded-15 text-center detail-berita" style="background-position: center; background-size: cover; background-image: url('<?= $b->gambar != '' || $b->gambar != NULL ? $b->gambar : 'kosong' ?>');">
-                            </figure>
+                                <div class="row mb-4 mx-2">
+                                    <div class="col-12">
+                                        <p class="my-2 title-1-home"><?= $b->judul ?></p>
+                                        <p class="text-secondary deskripsi-berita"><?= $b->keterangan ?><a href="<?= base_url('home/detail_berita/') . $b->id_konten ?>" class="label-merah">Selengkapnya</a></p>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                    </div>
 
-                    <div class="row mb-4 mx-2">
-                        <div class="col-12">
-                            <p class="my-2 title-1-home"><?= $b->judul ?></p>
-                            <p class="text-secondary deskripsi-berita"><?= $b->keterangan ?><a href="<?= base_url('home/detail_berita/') . $b->id_konten ?>" class="label-merah">Selengkapnya</a></p>
-                        </div>
-                    </div>
-                </a>
-            <?php endforeach; ?>
+                    <?php endforeach; ?>
+                </div>
+
+            <?php else : ?>
+                <?= vector_default("vector_tugas_kosong.svg", "Berita tidak tersedia!", "Sekolah tidak menyediakan berita untuk anda, Silahkan hubungi pihak sekolah jika terjadi kesalahan!"); ?>
+            <?php endif; ?>
         </div>
     </div>
     <div class="col-12 col-md-6 col-lg-4">
