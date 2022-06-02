5003be86e2c60a5d44e3555525381144bd133ffa
saka_mawa <sakadana2003@gmail.com>
2022-06-02 13:54:15 +0700

    Update

--- modules/profil/views/display_laporan.php
+++ modules/profil/views/display_laporan.php
@@ -2,41 +2,46 @@
     <h6 class="pt-3 ps-3 my-2"><?= $tanggal; ?></h6>
     <?php if ($result->mapel) : ?>
         <h6 class="pt-1 ps-3 mb-2">Presensi Harian</h6>
-        <a class="card mb-3 mx-2">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-auto">
-                        <div class="avatar avatar-50 shadow-sm rounded-15 avatar-presensi-outline">
-                            <div class="avatar avatar-40 rounded-15 avatar-presensi-inline">
-                                <i class="fa-solid fa-calendar-range size-20 text-white"></i>
+        <div class="row">
+            <div class="col-6 ps-4 pe-1">
+                <a class="card mb-3">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-auto">
+                                <div class="avatar avatar-50 shadow-sm rounded-15 avatar-presensi-outline">
+                                    <div class="avatar avatar-40 rounded-15 avatar-presensi-inline">
+                                        <i class="fa-solid fa-calendar-range size-20 text-white"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col align-self-center ps-0">
+                                <p class="mb-0 size-14 fw-medium">Scan Masuk</p>
+                                <p class="mb-0 size-12 fw-normal"><?= ifnull($result->presensi_siswa->scan_masuk, ' - '); ?></p>
                             </div>
                         </div>
                     </div>
-                    <div class="col align-self-center ps-0">
-                        <p class="mb-0 size-15 fw-medium">Scan Masuk</p>
-                        <p class="mb-0 size-13 fw-normal"><?= ifnull($result->presensi_siswa->scan_masuk, ' - '); ?></p>
-                    </div>
-                </div>
+                </a>
             </div>
-        </a>
-
-        <a class="card mb-3 mx-2">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-auto">
-                        <div class="avatar avatar-50 shadow-sm rounded-15 avatar-presensi-outline">
-                            <div class="avatar avatar-40 rounded-15 avatar-presensi-inline">
-                                <i class="fa-solid fa-calendar-range size-20 text-white"></i>
+            <div class="col-6 ps-1 pe-4">
+                <a class="card mb-3">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-auto">
+                                <div class="avatar avatar-50 shadow-sm rounded-15 avatar-presensi-outline">
+                                    <div class="avatar avatar-40 rounded-15 avatar-presensi-inline">
+                                        <i class="fa-solid fa-calendar-range size-20 text-white"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col align-self-center ps-0">
+                                <p class="mb-0 size-14 fw-medium">Scan Pulang</p>
+                                <p class="mb-0 size-12 fw-normal"><?= ifnull($result->presensi_siswa->scan_pulang, ' - '); ?></p>
                             </div>
                         </div>
                     </div>
-                    <div class="col align-self-center ps-0">
-                        <p class="mb-0 size-15 fw-medium">Scan Pulang</p>
-                        <p class="mb-0 size-13 fw-normal"><?= ifnull($result->presensi_siswa->scan_pulang, ' - '); ?></p>
-                    </div>
-                </div>
+                </a>
             </div>
-        </a>
+        </div>
         <h6 class="pt-1 ps-3 mb-2">Presensi Mata Pelajaran</h6>
         <?php foreach ($result->mapel as $row) : ?>
             <a class="card mb-3 mx-2">
