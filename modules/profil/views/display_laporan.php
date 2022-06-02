050a351717b5dc2f1cbd0520e709edc10f5d9fec
jeje <jjeje1303@gmail.com>
2022-06-02 12:04:33 +0700

    Update display_laporan.php

--- modules/profil/views/display_laporan.php
+++ modules/profil/views/display_laporan.php
@@ -1,7 +1,46 @@
 <div class="col-12">
     <h5><?= $tanggal; ?></h5>
-    <?php if ($result) : ?>
-        <?php foreach ($result as $row) : ?>
+
+
+    <?php if ($result->mapel) : ?>
+        <h4>PRESENSI HARIAN</h4>
+        <a class="card mb-3">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-auto">
+                        <div class="avatar avatar-60 shadow-sm rounded-18 avatar-presensi-outline">
+                            <div class="avatar avatar-50 rounded-18 avatar-presensi-inline">
+                                <i class="fa-solid fa-calendar-range size-26 text-white"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col align-self-center ps-0">
+                        <p class="mb-0 size-15 fw-medium">Scan Masuk</p>
+                        <p class="mb-0 size-13 fw-normal"><?= ifnull($result->presensi_siswa->scan_masuk, ' - '); ?></p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a class="card mb-3">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-auto">
+                        <div class="avatar avatar-60 shadow-sm rounded-18 avatar-presensi-outline">
+                            <div class="avatar avatar-50 rounded-18 avatar-presensi-inline">
+                                <i class="fa-solid fa-calendar-range size-26 text-white"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col align-self-center ps-0">
+                        <p class="mb-0 size-15 fw-medium">Scan Pulang</p>
+                        <p class="mb-0 size-13 fw-normal"><?= ifnull($result->presensi_siswa->scan_pulang, ' - '); ?></p>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <h4>PRESENSI MAPEL</h4>
+        <?php foreach ($result->mapel as $row) : ?>
             <a class="card mb-3">
                 <div class="card-body">
                     <div class="row">
