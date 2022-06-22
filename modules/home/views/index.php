1dc5499e4ed5f4e37cc88dbe2d6c8973fc96acef
jeje <jjeje1303@gmail.com>
2022-06-22 21:18:42 +0700

    carousel

--- modules/home/views/index.php
+++ modules/home/views/index.php
@@ -7,23 +7,57 @@
             <p class="mb-0 ms-2 title-1-home">Selamat Datang</p>
         </div>
     </div>
-    <div class="row mb-4 d-flex justify-content-center align-items-center me-2" style="margin-bottom : 0.75rem;" id="jadwal-list">
-        <?php if (count($data_siswa) > 1) : ?>
+    <?php if (count($data_siswa) > 1) : ?>
+        <div class="row mb-4 d-flex justify-content-center align-items-center me-2" style="margin-bottom : 0.75rem;" id="anak-list">
             <?php foreach ($data_siswa as $siswa) : ?>
-            <div class="col-6 mb-3">
-                <a href="<?= base_url('rapot') ?><?= "/{$siswa->id_siswa}" ?>" class="card mx-1 anak-wali jadwal">
-                    <div class="card-body">
-                        <div class="row mt-3 d-flex justify-content-center align-items-center">
-                            <div class="col-auto">
-                                <div class="avatar avatar-90 bg-opac-50 p-1 shadow-sm rounded-circle anak-wali" style="background-image: url(<?= $siswa->foto; ?>);"></div>
+                <div class="col-6 mb-3 anak">
+                    <a href="<?= base_url('rapot') ?><?= "/{$siswa->id_siswa}" ?>" class="card mx-1 anak-wali jadwal">
+                        <div class="card-body">
+                            <div class="row mt-3 d-flex justify-content-center align-items-center">
+                                <div class="col-auto">
+                                    <div class="avatar avatar-90 bg-opac-50 p-1 shadow-sm rounded-circle anak-wali" style="background-image: url(<?= $siswa->foto; ?>);"></div>
+                                </div>
+                            </div>
+                            <div class="row mt-3 d-flex justify-content-center align-items-center">
+                                <div class="col align-self-center ps-0">
+                                    <p class="size-14 text-center text-secondary mb-0"><?= $siswa->nis ?></p>
+                                    <p class="text-center label-merah fw-medium">
+                                        <?php
+                                        $exNama = explode(' ', $siswa->nama);
+                                        $gabung = $exNama[0];
+                                        if (isset($exNama[1])) {
+                                            $gabung .= ' ' . $exNama[1];
+                                        }
+
+                                        echo $gabung;
+                                        ?>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                        <div class="row mt-3 d-flex justify-content-center align-items-center">
-                            <div class="col align-self-center ps-0">
-                                <p class="size-14 text-center text-secondary mb-0"><?= $siswa->nis ?></p>
-                                <p class="text-center label-merah fw-medium">
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <nav aria-label=...>
+            <ul class="pagination pagination-carousel">
+                <a class="to-page d-none" href="javascript:void(0)"></a>
+            </ul>
+        </nav>
+    <?php else : ?>
+        <div class="row mb-4 d-flex justify-content-center align-items-center me-2" style="margin-bottom : 0.75rem;" id="jadwal-list">
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                <a href="<?= base_url('rapot/' . $data_siswa[0]->id_siswa) ?>" class="card mx-1 anak-wali-single" style="background-image: url(<?= base_url(); ?>assets/images/bg-card-wali.png); width: 100%;">
+                    <div class="card-body">
+                        <div class="row d-flex justify-content-center align-items-center">
+                            <div class="col-auto d-flex justify-content-center align-items-center">
+                                <div class="avatar avatar-90 bg-opac-50 p-1 shadow-sm rounded-circle anak-wali" style="background-image: url(<?= $data_siswa[0]->foto; ?>)"></div>
+                            </div>
+                            <div class="col align-self-center ps-3">
+                                <p class="text-start fw-medium size-15 mb-0">Nama Siswa</p>
+                                <p class="text-start label-merah fw-medium size-14">
                                     <?php
-                                    $exNama = explode(' ', $siswa->nama);
+                                    $exNama = explode(' ', $data_siswa[0]->nama);
                                     $gabung = $exNama[0];
                                     if (isset($exNama[1])) {
                                         $gabung .= ' ' . $exNama[1];
@@ -32,49 +66,18 @@
                                     echo $gabung;
                                     ?>
                                 </p>
+
+                                <p class="text-start fw-medium size-15 mb-0">NIS Siswa</p>
+                                <p class="text-start text-secondary size-14 mb-0"><?= $data_siswa[0]->nis ?></p>
                             </div>
                         </div>
                     </div>
                 </a>
             </div>
-            <?php endforeach; ?>
-        <?php else : ?>
-        <div class="col-12 d-flex justify-content-center align-items-center">
-            <a href="<?= base_url('rapot/' . $data_siswa[0]->id_siswa) ?>" class="card mx-1 anak-wali-single" style="background-image: url(<?= base_url(); ?>assets/images/bg-card-wali.png); width: 100%;">
-                <div class="card-body">
-                    <div class="row d-flex justify-content-center align-items-center">
-                        <div class="col-auto d-flex justify-content-center align-items-center">
-                            <div class="avatar avatar-90 bg-opac-50 p-1 shadow-sm rounded-circle anak-wali" style="background-image: url(<?= $data_siswa[0]->foto; ?>)"></div>
-                        </div>
-                        <div class="col align-self-center ps-3">
-                            <p class="text-start fw-medium size-15 mb-0">Nama Siswa</p>
-                            <p class="text-start label-merah fw-medium size-14">
-                                <?php
-                                $exNama = explode(' ', $data_siswa[0]->nama);
-                                $gabung = $exNama[0];
-                                if (isset($exNama[1])) {
-                                    $gabung .= ' ' . $exNama[1];
-                                }
-
-                                echo $gabung;
-                                ?>
-                            </p>
-
-                            <p class="text-start fw-medium size-15 mb-0">NIS Siswa</p>
-                            <p class="text-start text-secondary size-14 mb-0"><?= $data_siswa[0]->nis ?></p>
-                        </div>
-                    </div>
-                </div>
-            </a>
         </div>
-        <?php endif; ?>  
-    </div>
+    <?php endif; ?>
 
-    <nav aria-label=...>
-        <ul class="pagination pagination-carousel">
-            <a class="to-page d-none" href="javascript:void(0)"></a>
-        </ul>
-    </nav>
+
 
     <!-- Pengumuman -->
     <?php if (count($data_pengumuman) > 0) : ?>
