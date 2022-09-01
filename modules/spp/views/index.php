a2b8887ebeeda258f483d4abadb879b64eb95e88
saka_mawa <sakadana2003@gmail.com>
2022-09-01 13:55:06 +0700

    Push

--- modules/spp/views/index.php
+++ modules/spp/views/index.php
@@ -442,6 +442,9 @@
                                             } ?>>Belum Lunas</option>
                     </select>
                 </div>
+                <div class="d-flex justify-content-end align-items-center">
+                    <button class="btn btn-danger btn-sm btn-reset">Reset</button>
+                </div>
             </div>
             <div class="modal-footer border-0">
                 <button type="submit" class="btn btn-block btn-md btn-danger btn-filter">Tampilkan</button>
